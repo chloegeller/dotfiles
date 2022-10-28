0165501dc26f78ecf8a52a42b91af6341af7fa46
Chloe Geller <chloegeller@users.noreply.github.com>
2022-10-28 14:03:20 -0400

    updating .config files to work on linux

--- dot_config/private_zsh/conf.d/private_02-zstyles.zsh
+++ dot_config/private_zsh/conf.d/private_02-zstyles.zsh
@@ -2,11 +2,3 @@
 
 zstyle :omz:plugins:ssh-agent agent-forwarding on
 
-keys=("personal")
-identities=()
-for key in $keys; do
-    key="${key}.pem"
-    [ -f ${HOME}/.ssh/${key} ] && identities+=($key)
-done
-
-zstyle :omz:plugins:ssh-agent identities ${identities}
